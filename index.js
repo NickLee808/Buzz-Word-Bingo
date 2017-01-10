0b18ce9eb3a792ea9f7c9e111f8776ad52e7a34c
Nick Lee <nicklee808@gmail.com>
2017-01-09 21:10:41 -1000

    POST setup working

--- index.js
+++ index.js
@@ -2,31 +2,32 @@
 
 console.log('Directory name:', __dirname);
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
-var app = express();
-let buzzwords = {
-  'buzzwords': [],
-  'success': false,
-};
+let app = express();
+let buzzwords = [];
 
 app.use(bodyParser.json());
 
+// GET /
 app.get('/', (req, res) =>
   res.sendFile(__dirname + '/index.html'));
 
+// GET /buzzwords
 app.get('/buzzwords', (req, res) =>
   res.json(buzzwords));
 
+// POST /buzzword
 app.post('/buzzword', (req, res) => {
-
+  buzzwords.push(req.body);
+  res.json(buzzwords);
   console.log(buzzwords);
-  res.send('asdf');
 });
 
-var server = app.listen(3000, () => {
-  var host = server.address().address;
-  var port = server.address().port;
+let server = app.listen(3000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
 });
